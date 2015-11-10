22ee8d96b8778b82337d316714a4b3a11908fb55
Sean Marshallsay <srm.1708@gmail.com>
2015-11-10 18:55:45 +0000

    Mildly better error handling.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use std::fs;
 use std::path;
 use std::process;
 
+use std::error::Error;
+
 macro_rules! try_process {
     ($output:expr, $command:expr) => (match $output {
         Ok(ref o) => if o.status.success() {
@@ -14,26 +16,30 @@ macro_rules! try_process {
             println!("`{}` failed: {}", $command, String::from_utf8_lossy(&o.stderr).trim());
             process::exit(o.status.code().unwrap_or(1));
         },
-        Err(_) => panic!(format!("`{}` could not be run", $command)),
-    })
+        Err(e) => panic!("`{}` could not be run: {}", $command, e.description()),
+    });
 }
 
-// TODO: STOP PANICKING! It's going to be Ok!
+macro_rules! msg_file_line {
+    ($msg:expr) => (concat!($msg, ":", file!(), ":", line!()));
+}
+
 fn main() {
-    let cellar = process::Command::new("brew").arg("--cellar").output();
-    let cellar = try_process!(cellar, "brew --cellar");
-    let cellar = path::Path::new(cellar.trim());
+    // This is hardly going to be a bottleneck but it's not needed until later so we can happily
+    // let it chug along in the background.
+    let cellar = process::Command::new("brew").arg("--cellar")
+        .spawn()
+        .expect(msg_file_line!("could not run `brew install`"));
 
     // Create a temporary directory to do the initial install into.
     // Randomly generated suffix is easier than the possibility of cleanup failing.
-    let temp_cargo_brew_dir = format!("cargo-brew-{}", rand::random::<u32>());
-    let temp_dir = env::temp_dir().join(temp_cargo_brew_dir);
-    fs::create_dir(&temp_dir).expect("could not create temporary directory");
+    let temp_dir = format!("cargo-brew-{}", rand::random::<u32>());
+    let temp_dir = env::temp_dir().join(temp_dir);
+    fs::create_dir(&temp_dir).expect(msg_file_line!("could not create temporary directory"));
     let args = set_root(env::args(), temp_dir.to_str().expect("non-unincode temporary directory"));
 
-    // TODO: we can run this and `brew --cellar` in parallel.
     // Install crate into temporary directory so that it can be moved to the Cellar later.
-    // Inherits stdout and stderr.
+    // Inherits stdout so user doesn't have to wait staring at a blank screen.
     let install = process::Command::new("cargo").arg("install").args(&args)
         .stdout(process::Stdio::inherit())
         .output();
@@ -57,21 +63,35 @@ fn main() {
         } else {
             panic!("second `cargo install` succeeded");
         },
-        Err(_) => panic!("`cargo install` could not be run"),
+        Err(e) => panic!("`cargo install` could not be run: {}", e.description()),
     };
 
+    // Find user's Homebrew Cellar and create the directory structure.
+    let cellar = cellar.wait_with_output();
+    let cellar = try_process!(cellar, "brew --cellar");
+    let cellar = path::Path::new(cellar.trim());
     let brew_root = cellar.join(&krate).join(vers).join("bin");
-    fs::create_dir_all(&brew_root).expect("could not create directories in Cellar");
-    // TODO: maybe use push() instead of join()? might be useful for better error messages.
+    fs::create_dir_all(&brew_root).expect(msg_file_line!("could not create directories in Cellar"));
+
     // Loop through "temp_dir/bin" and copy the files into "brew_root/bin".
-    match temp_dir.join("bin").read_dir() {
-        Ok(rd) => for f in rd {
-            // TODO: continue on Err but warn.
-            let f = f.expect("io error");
-            let name = f.file_name();
-            fs::rename(f.path(), brew_root.join(name)).expect("unable to move binary into Cellar");
+    let temp_dir = temp_dir.join("bin");
+    match temp_dir.read_dir() {
+        Ok(dir) => for file in dir {
+            let file = match file {
+                Ok(file) => file,
+                Err(e) => {
+                    println!("Warning: could not read directory entry: {}", e.description());
+                    continue
+                },
+            };
+            let name = file.file_name();
+            let old_path = file.path();
+            let new_path = brew_root.join(name);
+            if let Err(e) = fs::rename(&old_path, &new_path) {
+                println!("Warning: could not move binary '{:?}' to '{:?}': {}", old_path, new_path, e.description());
+            };
         },
-        Err(_) => panic!("unable to open temporary bin directory"),
+        Err(_) => panic!("unable to open '{:?}'", temp_dir),
     };
 
     let brew_link = process::Command::new("brew").arg("link").arg(&krate).output();
@@ -104,7 +124,7 @@ fn parse_krate_vers_from_error(err: &str) -> (String, String) {
     // Find the `$KRATE v$VERS part of the error message.
     let re = regex::Regex::new(r"`(\S+) v([0-9.]+)").expect("statically known regex is invalid");
     let krate_vers = if let Some(caps) = re.captures(err) {
-        let krate = caps.at(1).expect("could not determine crate name").to_owned();
+        let krate = caps.at(1).expect(msg_file_line!("could not determine crate name")).to_owned();
         let vers = caps.at(2).unwrap_or("HEAD").to_owned();
         (krate, vers)
     } else {
